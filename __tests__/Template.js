b78c6c05205dee5be8479281cc3eaaa7c564b328
CharlesBCookman <CharlesBCookman@gmail.com>
2023-02-10 09:09:25 -0800

    initial commit

---
+++ __tests__/Template.js
@@ -0,0 +1,9 @@
+import {TESTABLE} from './../src/Template.js';
+
+describe('TESTABLE', () => {
+
+  test('SHOULD TEST ---------', () => {
+    //CODE TO TEST
+    expect(TESTABLE.VARIABLE).toEqual("EXPECTED VALUE");
+  });
+});
